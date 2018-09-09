8ac1756db54e7ddeb35cf713889f8322ebbbd3cb
Jack <hui2@pdx.edu>
2018-09-09 14:40:18 -0700

    Initial commit

---
+++ .idea/Main.js
@@ -0,0 +1,26 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks,tabcontent1,tabcontent2,tabcontent3;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tabcontent1 = document.getElementsByClassName("tabcontent1");
+    for (i = 0; i < tabcontent1.length; i++) {
+        tabcontent1[i].style.display = "none";
+    }
+    tabcontent2 = document.getElementsByClassName("tabcontent2");
+    for (i = 0; i < tabcontent2.length; i++) {
+        tabcontent2[i].style.display = "none";
+    }
+    tabcontent3 = document.getElementsByClassName("tabcontent3");
+    for (i = 0; i < tabcontent3.length; i++) {
+        tabcontent3[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
